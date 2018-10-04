ae07d608f929f8e4d377fb440bb86a2dc219e307
Simon Situ <ssitu001@gmail.com>
2018-10-03 22:07:39 -0700

    add toggleFavorite function

--- src/Components/GiphyContainer/GiphyContainer.js
+++ src/Components/GiphyContainer/GiphyContainer.js
@@ -3,17 +3,24 @@ import React from 'react';
 import Giphy from '../Giphy/Giphy';
 import { Grid } from 'semantic-ui-react'
 
-const GiphyContainer = ({gifs}) => {
+const GiphyContainer = ({ addToFavorites, removeFromFavorites, gifs }) => {
 
-   let img = gifs.length ? gifs.map((gif) => {
+   const giphy = gifs.length ? gifs.map((gif, i) => {
       let gifUrl = gif.images.original.gif_url;
-      return <Giphy gifUrl={gifUrl} title={gif.title} key={gif.id}/>;
+      return (
+        <Giphy 
+          toggleFavorite={gif.isFavorite ? removeFromFavorites : addToFavorites} 
+          gifUrl={gifUrl} 
+          title={gif.title} 
+          key={`${gif.id} + ${i}`}
+        />
+      );
     }) : [];
 
   return (
       <Grid columns={4}>
         <Grid.Row  stretched>
-          {img}
+          {giphy}
         </Grid.Row>
       </Grid>
   )
