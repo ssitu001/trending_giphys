a87c3ccee28a83f47d5dc89697ca6e342be51416
Simon Situ <ssitu001@gmail.com>
2018-10-03 22:10:50 -0700

    add logic for storing favorites

--- src/Components/GiphysHome/GiphysHome.js
+++ src/Components/GiphysHome/GiphysHome.js
@@ -7,37 +7,104 @@ import GiphyContainer from '../GiphyContainer/GiphyContainer';
 
 import { Container, Menu, Header, Visibility, Dropdown, Image, Input, Form } from 'semantic-ui-react'
 
-
 class GiphysHome extends Component {
   state = {
     searchValue: '',
     loading: false,
-    gifs: []
+    gifs: [],
+    favorites: []
   }
 
   componentDidMount() {
-    client.trending('gifs', {})
+    window.addEventListener('scroll', this.handleScroll);
+    this.fetchGifs();
+  }
+
+  fetchGifs = () => {
+    const { gifs } = this.state;
+    const offset = gifs.length || 20;
+    client.trending('gifs', {offset})
       .then((gifs) => {
-        this.setState({gifs: gifs.data});
+        const storedFavorites = this.getStoredFavorites();
+        this.setState({ favorites: Object.values(storedFavorites), gifs: [ ...this.state.gifs, ...this.isFavorite(gifs.data) ]})
       })
       .catch((err) => {
-        console.err(err);
+        console.error(err);
       })
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);    
+  }
+
+  handleScroll = (e) => {
+    const scrollNode = document.scrollingElement || document.documentElement;
+    if (scrollNode.scrollHeight <= scrollNode.scrollTop + window.innerHeight) {
+      // _.throttle(this.fetchGifs, 100);
+      this.fetchGifs();
+    }
   }
 
   handleChange = (e) => {
     this.setState({searchValue: e.target.value});
   }
 
+  getStoredFavorites = () => JSON.parse(localStorage.getItem('favorites')) || {};
+
+  addFavoriteToLocalStorage = newFavorite => {
+    if (localStorage) {
+      const storedFavorites = this.getStoredFavorites();
+
+      localStorage.setItem('favorites', JSON.stringify({ ...storedFavorites, [newFavorite.id]: newFavorite }));
+    }
+  }
+
+  removeFavoriteFromLocalStorage = (id) => {
+    if (localStorage) {
+      const storedFavorites = this.getStoredFavorites();
+      if (storedFavorites.id) {
+        delete storedFavorites.id;
+      }
+
+      localStorage.setItem('favorites', JSON.stringify(storedFavorites));
+    }
+
+  }
+
+  isFavorite = (gifs) => {
+    if (localStorage) {
+      const storedFavorites = this.getStoredFavorites();
+
+      return gifs.map(gif => {
+        if (storedFavorites[gif.id]) {
+          gif.isFavorite = true;
+        }
+        return gif;
+      });
+    }
+  }
+
+  addToFavorites = (gif) => {
+    this.setState({ favorites: [ ...this.state.favorites, gif ] }, () => {
+     this.addFavoriteToLocalStorage(gif) 
+    });
+  }
+  
+  removeFromFavorites = (id) => {
+    this.setState({ favorites: this.state.favorites.filter(favorite => favorite.id === id) }, () => {
+      this.removeFavoriteFromLocalStorage(id);
+    });
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({loading: true});
-    client.search('gifs', {"q": this.state.searchValue})
+    this.setState({ loading: true });
+    client.search('gifs', { "q": this.state.searchValue })
     .then((gifs) => {
-      this.setState({loading: false, gifs: gifs.data, searchValue: ''});
+      this.setState({ loading: false, gifs: gifs.data, searchValue: '' });
     })
     .catch((err) => {
-      console.err(err);
+      console.error(err);
     });
   }
 
@@ -73,7 +140,7 @@ class GiphysHome extends Component {
           >
             <Container text>
               <Menu.Item>
-                <Form onSubmit={_.debounce(this.handleSubmit, 1000, {leading: true})}>
+                <Form onSubmit={_.debounce(this.handleSubmit, 300)}>
                   <Input loading={loading} icon='user' placeholder="Search for your giphy" onChange={this.handleChange} value={this.state.searchValue}/>
                 </Form>
               </Menu.Item>
@@ -101,7 +168,13 @@ class GiphysHome extends Component {
           </Menu>
         </Visibility>
         <Container>
-          <GiphyContainer gifs={gifs}/>
+          <GiphyContainer 
+            gifs={gifs}  
+            style={{ overflowY: 'auto' }} 
+            onScroll={this.handleScroll}
+            addToFavorites={this.addToFavorites}
+            removeFromFavorites={this.removeFromFavorites}
+          />
         </Container>
       </div>
     )
