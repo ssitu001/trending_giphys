45273914112c94787ba42abf619cd5be409bee17
Simon Situ <ssitu001@gmail.com>
2018-10-11 00:16:30 -0700

    add home styles

---
+++ src/Styles/Home.js
@@ -0,0 +1,18 @@
+export const menuStyle = {
+  backgroundColor: 'black',
+  border: 'none',
+  borderRadius: 0,
+  boxShadow: 'none',
+  marginBottom: '4em',
+  marginTop: '4em',
+  transition: 'box-shadow 0.5s ease, padding 0.5s ease',
+  display: 'flex', 
+  alignItems: 'center', 
+}
+
+export const fixedMenuStyle = {
+  backgroundColor: 'black',
+  boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+  display: 'flex', 
+  alignItems: 'center', 
+}
