e485561ef185aa7f61eadaf13faf4a85e2400d9e
Simon Situ <ssitu001@gmail.com>
2018-10-11 00:15:57 -0700

    add giphy home component

--- src/Components/GiphysHome/GiphysHome.js
+++ src/Components/GiphysHome/GiphysHome.js
@@ -4,74 +4,106 @@ import GphApiClient from 'giphy-js-sdk-core';
 const client = new GphApiClient('njJKySvRX2tLcXayN5ep5vXmHoJ6h1It');
 
 import GiphyContainer from '../GiphyContainer/GiphyContainer';
+import Banner from '../Banner/Banner';
 
-import { Container, Menu, Header, Visibility, Button, Input, Form } from 'semantic-ui-react'
+import { menuStyle, fixedMenuStyle } from '../../Styles/Home';
+import { CONSTANTS } from '../../Constants/constants';
 
-const CONSTANTS = {
-  HOME: 'Home',
-  FAVORITES: 'Favorites'
-};
-
-const menuStyle = {
-  border: 'none',
-  borderRadius: 0,
-  boxShadow: 'none',
-  marginBottom: '1em',
-  marginTop: '4em',
-  transition: 'box-shadow 0.5s ease, padding 0.5s ease',
-}
-
-const fixedMenuStyle = {
-  backgroundColor: '#fff',
-  border: '1px solid #ddd',
-  boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
-}
+import { Container, Menu, Header, Visibility, Button, Input, Form, Icon } from 'semantic-ui-react'
 
 class GiphysHome extends Component {
-  state = {
-    searchValue: '',
-    loading: false,
-    currentView: CONSTANTS.HOME,
-    gifs: [],
-    favorites: [],
-    menuFixed: false,
-  }
+    state = {
+      searchValue: '',
+      currentSearchValue: '',
+      loading: false,
+      currentView: CONSTANTS.HOME,
+      currentState: CONSTANTS.TRENDING,
+      gifs: [],
+      favorites: [],
+      menuFixed: false,
+    }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-    this.fetchGifs();
+    window.addEventListener('scroll', _.throttle(this.handleScroll, 1000));
+    this.fetchTrendingGifs();
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);    
   }
 
-  fetchGifs = () => {
+  fetchTrendingGifs = () => {
     const { gifs } = this.state;
-    const offset = gifs.length || 20;
-    client.trending('gifs', {offset})
-      .then((gifs) => {
+    const offset = gifs.length ? gifs.length + 10 : 0;
+
+    client.trending('gifs', {limit: 10, offset: offset})
+      .then((newGifs) => {
         const storedFavorites = this.getStoredFavorites();
-        this.setState({ favorites: Object.values(storedFavorites), gifs: [ ...this.state.gifs, ...this.isFavorite(gifs.data) ]})
+        this.setState({ 
+          favorites: Object.values(storedFavorites), 
+          gifs: [ ...gifs, ...this.isFavorite(newGifs.data) ],
+        });
       })
       .catch((err) => {
         console.error(err);
       })
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);    
+  fetchAdditionalSearchedGifs = () => {
+    const { currentSearchValue, gifs } = this.state;
+
+    client.search('gifs', { "q": currentSearchValue, limit: 10, offset: gifs.length + 10 })
+      .then((newGifs) => {
+        const storedFavorites = this.getStoredFavorites();
+        this.setState({ 
+          favorites: Object.values(storedFavorites),
+          gifs: [...gifs, ...this.isFavorite(newGifs.data) ],
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
-  handleScroll = (e) => {
+  handleSubmit = () => {
+    const { searchValue } = this.state;
+
+    this.setState({ 
+      loading: true, 
+      currentState: CONSTANTS.USER_SEARCH, 
+      currentSearchValue: searchValue 
+    });
+
+    client.search('gifs', { "q": searchValue, limit: 10, offset: 0 })
+    .then((gifs) => {
+      const storedFavorites = this.getStoredFavorites();
+      this.setState({ 
+        loading: false, 
+        favorites: Object.values(storedFavorites), 
+        gifs: gifs.data, 
+        searchValue: '' 
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  }
+
+  handleScroll = () => {
+    const { currentState } = this.state;
     const scrollNode = document.scrollingElement || document.documentElement;
     if (scrollNode.scrollHeight <= scrollNode.scrollTop + window.innerHeight) {
-      // _.throttle(this.fetchGifs, 100);
-      this.fetchGifs();
+      return currentState === CONSTANTS.TRENDING 
+        ? this.fetchTrendingGifs() 
+        : this.fetchAdditionalSearchedGifs();
     }
   }
 
-  handleChange = (e) => {
-    this.setState({searchValue: e.target.value});
-  }
-
-  getStoredFavorites = () => JSON.parse(localStorage.getItem('favorites')) || {};
+  handleChange = (e) => this.setState({ searchValue: e.target.value });
+  
+  getStoredFavorites = () => {
+    return JSON.parse(localStorage.getItem('favorites')) || {};
+  } 
 
   addFavoriteToLocalStorage = (newFavorite) => {
     if (localStorage) {
@@ -121,40 +153,42 @@ class GiphysHome extends Component {
     });
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({ loading: true });
-    client.search('gifs', { "q": this.state.searchValue })
-    .then((gifs) => {
-      this.setState({ loading: false, gifs: gifs.data, searchValue: '' });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  }
-
   stickTopMenu = () => this.setState({ menuFixed: true });
 
   unStickTopMenu = () => this.setState({ menuFixed: false });
 
-  get currentView() {
-    console.log('calld')
-    return this.state.currentView === CONSTANTS.HOME
-    ? this.state.gifs
-    : this.state.favorites
+  get getCurrentState() {
+    const { currentState, gifs, favorites } = this.state;
+    return currentState !== CONSTANTS.FAVORITES
+      ? gifs
+      : favorites;
+  }
+
+  setCurrentState = () => {
+    const { currentSearchValue } = this.state;
+    if ( currentSearchValue && currentSearchValue.length ) {
+      this.setState({ currentState: CONSTANTS.USER_SEARCH });
+    } else {
+      this.setState({ currentState: CONSTANTS.TRENDING });
+    }
   }
 
   render() {
-    let {gifs, favorites, loading, currentView, menuFixed} = this.state;
+    const { 
+      loading,
+      searchValue, 
+      currentState,
+      menuFixed, 
+      currentSearchValue
+    } = this.state;
 
     return (
-
       <div>
-        <Container text style={{ marginTop: '2em' }}>
-          <Header as='h1'>Giphy Me</Header>
-          <p>
-            Lorem isum something something
-          </p>
+        <Container style={{ marginTop: '2em'}}>
+          <Header size='huge' color={'grey'}>
+            <Icon size='huge' name='images outline'/>GIPHY ME
+          </Header>
+          <Header size='medium' color={'grey'}>Just for fun...</Header>
         </Container>
         <Visibility
           onBottomPassed={this.stickTopMenu}
@@ -166,37 +200,40 @@ class GiphysHome extends Component {
             fixed={menuFixed ? 'top' : null}
             style={menuFixed ? fixedMenuStyle : menuStyle}
           >
-            <Container text>
+            <Container>
               <Menu.Item>
                 <Form onSubmit={_.debounce(this.handleSubmit, 300)}>
-                  <Input loading={loading} icon='search' placeholder="Search for your giphy" onChange={this.handleChange} value={this.state.searchValue}/>
+                  <Input loading={loading} icon='search' placeholder="Search for all the gifs" onChange={this.handleChange} value={searchValue}/>
                 </Form>
               </Menu.Item>
 
               <Menu.Menu position='right'>
-              <div>
-                <Button 
-                  active={true} 
-                  onClick={() => {this.setState({ currentView: CONSTANTS.HOME })}} 
-                  circular 
-                  icon='home' 
-                /> 
-                <Button 
-                  active={false} 
-                  onClick={() => {this.setState({ currentView: CONSTANTS.FAVORITES })}} 
-                  circular 
-                  icon='heart' 
-                /> 
-              </div>
+                <Menu.Item>
+                  <Button
+                    active={ currentState !== CONSTANTS.FAVORITES } 
+                    onClick={this.setCurrentState} 
+                    circular 
+                    icon='home' 
+                  />
+                  <Button 
+                    active={ currentState === CONSTANTS.FAVORITES } 
+                    onClick={() => {this.setState({ currentState: CONSTANTS.FAVORITES })}} 
+                    circular 
+                    icon='heart' 
+                  /> 
+                </Menu.Item>
               </Menu.Menu>
             </Container>
           </Menu>
         </Visibility>
         <Container>
+          <Banner 
+            currentSearchValue={currentSearchValue}
+            currentState={currentState}
+          />
           <GiphyContainer 
-            gifs={this.currentView}  
+            gifs={this.getCurrentState}  
             style={{ overflowY: 'auto' }} 
-            onScroll={this.handleScroll}
             addToFavorites={this.addToFavorites}
             removeFromFavorites={this.removeFromFavorites}
           />
